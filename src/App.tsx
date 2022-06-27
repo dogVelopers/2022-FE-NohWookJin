83dd1d2a0e93ec30c8e11d88790acd1b8e5fb1c2
NohWookJin <jinn98@kakao.com>
2022-06-27 11:09:26 +0900

    apply styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
 function App() {
-  // opt + sft + f = 자동 수정
-  const a = '1';
+  const [number, setNumber] = useState<number>(1);
 
   return (
     <div className="App">
-      <h1>안녕하세용!</h1>
+      <StyledH1 num={number} backgroundColor="green">
+        안녕하세요
+      </StyledH1>
+      <p>{number}</p>
+      <button
+        onClick={() => {
+          setNumber(prev => prev + 1);
+        }}
+      >
+        up
+      </button>
     </div>
   );
 }
 
 export default App;
+
+interface styledH1Props {
+  num: number;
+  backgroundColor: string;
+}
+
+const StyledH1 = styled.h1<styledH1Props>`
+  color: ${({ num }) => (num % 2 == 0 ? 'red' : 'blue')};
+  font-size: ${props => props.num}px;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+`;
