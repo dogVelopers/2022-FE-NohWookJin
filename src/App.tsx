ac1cd22c90357893692d456f9205e82282e31fde
NohWookJin <jinn98@kakao.com>
2022-07-03 22:33:27 +0900

    feat: add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,11 @@
-import { useState } from 'react';
-import styled from 'styled-components';
+import InputSection from './components/InputSection';
+import ListSection from './components/ListSection';
 
-function App() {
-  const [number, setNumber] = useState<number>(1);
-
+export default function App() {
   return (
-    <div className="App">
-      <StyledH1 num={number} backgroundColor="green">
-        안녕하세요
-      </StyledH1>
-      <p>{number}</p>
-      <button
-        onClick={() => {
-          setNumber(prev => prev + 1);
-        }}
-      >
-        up
-      </button>
+    <div>
+      <InputSection />
+      <ListSection />
     </div>
   );
 }
-
-export default App;
-
-interface styledH1Props {
-  num: number;
-  backgroundColor: string;
-}
-
-const StyledH1 = styled.h1<styledH1Props>`
-  color: ${({ num }) => (num % 2 == 0 ? 'red' : 'blue')};
-  font-size: ${props => props.num}px;
-  background-color: ${({ backgroundColor }) => backgroundColor};
-`;
