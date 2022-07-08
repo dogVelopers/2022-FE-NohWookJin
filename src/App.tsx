97f1d9f1e39c63af4ae29dad7d7ac93ed8df0f8a
NohWookJin <jinn98@kakao.com>
2022-07-08 19:42:48 +0900

    feat: Add declaration

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import InputSection from './components/InputSection';
 import ListSection from './components/ListSection';
+import useTodos from './hooks/api/useTodos';
 
 export default function App() {
+  const { createTodo, todos } = useTodos();
+
   return (
     <div>
-      <InputSection />
-      <ListSection />
+      <InputSection createTodo={createTodo} />
+      <ListSection todos={todos} />
     </div>
   );
 }
