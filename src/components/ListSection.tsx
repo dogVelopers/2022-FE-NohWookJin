1db0dd46107841913fd28bee9d1e8785e91a751f
NohWookJin <jinn98@kakao.com>
2022-07-14 14:03:40 +0900

    style: ListSection.tsx

--- src/components/ListSection.tsx
+++ src/components/ListSection.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
 interface Props {
   todos: ITodo[];
@@ -6,10 +7,25 @@ interface Props {
 
 export default function ListSection({ todos }: Props) {
   return (
-    <section>
+    <StyledSection>
       {todos.map(todo => (
-        <p key={todo.id}>{todo.fields.name}</p>
+        <Styledp key={todo.id}>{todo.fields.name}</Styledp>
       ))}
-    </section>
+    </StyledSection>
   );
 }
+
+const StyledSection = styled.section`
+  background-color: rgb(240, 240, 180);
+  border-radius: 1.3rem;
+  padding: 0.5rem 1rem;
+  margin-top: 0rem;
+  width: 50vw;
+  height: 100vh;
+`;
+
+const Styledp = styled.p`
+  border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+  padding: 0.2rem 0.1rem;
+  font-size: 0.9rem;
+`;
