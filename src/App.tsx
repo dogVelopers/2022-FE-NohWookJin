13c0ecb24149dc74303573740959325289297e70
NohWookJin <jinn98@kakao.com>
2022-07-14 14:01:06 +0900

    style: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import InputSection from './components/InputSection';
 import ListSection from './components/ListSection';
 import useTodos from './hooks/api/useTodos';
+import styled from 'styled-components';
 
 export default function App() {
   const { createTodo, todos } = useTodos();
 
   return (
-    <div>
-      <InputSection createTodo={createTodo} />
+    <StyledMain>
+      <h2>ToDo 🐶</h2>
       <ListSection todos={todos} />
-    </div>
+      <InputSection createTodo={createTodo} />
+    </StyledMain>
   );
 }
+
+const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: whitesmoke;
+  height: 100vh;
+`;
