ce08d07c895b87bea83a8f0afd6d821b9043eeb7
NohWookJin <jinn98@kakao.com>
2022-07-14 14:03:23 +0900

    style: InputSection.tsx

--- src/components/InputSection.tsx
+++ src/components/InputSection.tsx
@@ -1,5 +1,6 @@
 import { FormEvent } from 'react';
 import useTextInput from '../hooks/uesTextInput';
+import styled from 'styled-components';
 
 interface Props {
   createTodo: (value: string) => void;
@@ -15,11 +16,31 @@ export default function InputSection({ createTodo }: Props) {
   }
 
   return (
-    <section>
-      <form onSubmit={onSubmit}>
-        <input value={value} onChange={onChange} />
-        <button>저장</button>
-      </form>
-    </section>
+    <StyledSection>
+      <StyledForm onSubmit={onSubmit}>
+        <StyledInput placeholder="오늘도 화이팅! 🏃" value={value} onChange={onChange} />
+        <StyledButton>저장</StyledButton>
+      </StyledForm>
+    </StyledSection>
   );
 }
+
+const StyledSection = styled.section`
+  margin-top: 1rem;
+`;
+const StyledForm = styled.form`
+  position: relative;
+  bottom: 3.8rem;
+`;
+const StyledInput = styled.input`
+  width: 48vw;
+  margin: 0rem 0.2rem;
+  padding: 0.5rem 0.4rem;
+  border: none;
+  border-radius: 0.2rem;
+  background-color: whitesmoke;
+`;
+const StyledButton = styled.button`
+  border: none;
+  background-color: rgb(240, 240, 180);
+`;
